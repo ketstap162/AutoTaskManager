d666d754cbac4f85fa6e4cbdaecedd8ef1464da3
Zakhar Podporinov <zp162162@gmail.com>
2023-09-04 18:05:05 +0300

    Add register_view and update redirect_to_home

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,23 @@
-from django.shortcuts import redirect
+from django.contrib.auth import login, authenticate
+from django.shortcuts import redirect, render
+
+from application.forms import EmployeeForm
 
 
 def redirect_to_home(request):
-    return redirect('/home')
+    return redirect("Application:Home")
+
+
+def register_view(request):
+    if request.method == "POST":
+        form = EmployeeForm(request.POST)
+        if form.is_valid():
+            form.save()
+            email = form.cleaned_data.get("email")
+            password = form.cleaned_data.get("password")
+            user = authenticate(request, email=email, password=password)
+            return redirect("Application:Home")
+
+    else:
+        form = EmployeeForm()
+    return render(request, "registration/register.html", {"form": form})
