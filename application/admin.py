f2fbacd9dc82004b79fc52be1db412b13286a1d2
Zakhar Podporinov <zp162162@gmail.com>
2023-09-04 18:04:12 +0300

    Register models in admin.py

--- application/admin.py
+++ application/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from application.models import Employee, Order
 
 # Register your models here.
+
+
+@admin.register(Employee)
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = ["id", "name", "email", "position", "probation"]
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ["id", "name", "employee"]
