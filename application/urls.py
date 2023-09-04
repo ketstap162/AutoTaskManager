1437dffd882e6be94d1b5c419240a1f521ff402e
Zakhar Podporinov <zp162162@gmail.com>
2023-09-04 17:02:48 +0300

    Add delete task URL pattern

--- application/urls.py
+++ application/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from application.views import home_page_view
+from application.views import home_page_view, delete_task_view
 
 app_name = "Application"
 
 
 urlpatterns = [
-    path("", home_page_view, name="Home")
+    path("", home_page_view, name="Home"),
+    path("<int:pk>/delete", delete_task_view, name="OrderDelete")
 ]
