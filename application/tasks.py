4d423c44bbbb33470621160c81dfd05121067ac2
Zakhar Podporinov <zp162162@gmail.com>
2023-09-04 13:37:39 +0300

    Update Task Structure. Connect process to Django ORM.

--- application/tasks.py
+++ application/tasks.py
@@ -1,19 +1,28 @@
 import multiprocessing
+import os
 import time
+
+import django
 
 
 class TaskAddManager:
     def __init__(self):
-        self.process = multiprocessing.Process(target=self.regular_task_add)
+        self.process = None
 
     @staticmethod
     def regular_task_add():
-        while True:
-            print("Process iteration started")
-            time.sleep(2)
-            print("Proces iteration ended")
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "main_app.settings")
+        django.setup()
+
+        from application.models import Employee
+        employees = Employee.objects.all()
+
+        with open("employees.txt", "a") as file:
+            for employee in employees:
+                file.write(str(employee) + "\n")
 
     def start(self) -> None:
+        self.process = multiprocessing.Process(target=self.regular_task_add, daemon=True)
         self.process.start()
 
     def is_process_alive(self) -> bool:
@@ -27,3 +36,6 @@ if __name__ == "__main__":
     task_manager = TaskAddManager()
     task_manager.start()
 
+    time.sleep(10)
+
+    task_manager.stop()
